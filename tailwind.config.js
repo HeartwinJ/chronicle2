6c7867e0294470b8fe3e228de35f9d78b82dac09
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2022-10-09 14:35:58 +0400

    Updated Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["'Montserrat'", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
